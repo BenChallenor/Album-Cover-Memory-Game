644338cb8f0da314c1a93aae28c3581550e63890
Ben <ben.challenor@btopenworld.com>
2018-08-24 18:28:30 +0100

    Add grid class for cards to append to

--- main.js
+++ main.js
@@ -1,49 +1,72 @@
 const cardsArray = [{
-    'name': 'purple-rain'
-    'img': 'images/purplerain.jpeg'
+    'name': 'purple-rain',
+    'img': 'images/purplerain.jpg'
   },
   {
-    'name': '1999'
-    'img': 'images/1999.jpeg'
+    'name': '1999',
+    'img': 'images/1999.jpg'
   },
   {
-    'name': 'around-the-world-in-a-day'
-    'img': 'images/aroundtheworldinaday.jpeg'
+    'name': 'around-the-world-in-a-day',
+    'img': 'images/aroundtheworldinaday.jpg'
   },
   {
-    'name': 'controversy'
-    'img': 'images/contoversy.jpeg'
+    'name': 'controversy',
+    'img': 'images/controversy.jpg'
   },
   {
-    'name': 'diamonds-and-pearls'
-    'img': 'images/diamondsandpearls.jpeg'
+    'name': 'diamonds-and-pearls',
+    'img': 'images/diamondsandpearls.jpg'
   },
   {
-    'name': 'dirty-mind'
-    'img': 'images/dirtymind.jpeg'
+    'name': 'dirty-mind',
+    'img': 'images/dirtymind.jpg'
   },
   {
-    'name': 'graffiti-bridge'
-    'img': 'images/graffitibridge.jpeg'
+    'name': 'graffiti-bridge',
+    'img': 'images/graffitibridge.jpg'
   },
   {
-    'name': 'lovesexy'
-    'img': 'images/lovesexy.jpeg'
+    'name': 'lovesexy',
+    'img': 'images/lovesexy.jpg'
   },
   {
-    'name': 'parade'
-    'img': 'images/parade.jpeg'
+    'name': 'parade',
+    'img': 'images/parade.jpg'
   },
   {
-    'name': 'prince'
-    'img': 'images/prince.jpeg'
+    'name': 'prince',
+    'img': 'images/prince.jpg'
   },
   {
-    'name': 'sign-o-the-times'
-    'img': 'images/signothetimes.jpeg'
+    'name': 'sign-o-the-times',
+    'img': 'images/signothetimes.jpg'
   },
   {
-    'name': 'symbol'
-    'img': 'images/symbol.jpeg'
+    'name': 'symbol',
+    'img': 'images/symbol.jpg'
   },
 ];
+
+// grabs the game root id
+const game = document.getElementById('game');
+
+// creates a section with a grid class
+const grid = document.createElement('section');
+grid.setAttribute("class", "grid");
+
+// adds the grid section to the game div to the DOM
+game.appendChild(grid);
+
+cardsArray.forEach(item => {
+  // create a div
+  const card = document.createElement("div");
+  // apply a card class to that div
+  card.classList.add("card");
+  // sets the data-name attribute of the div to the cardsArray
+  card.dataset.name = item.name;
+  // applies the bacground image of the div to the cardsArray
+  card.style.backgroundImage = `url(${item.img})`;
+  // adds the div to the grid section
+  grid.appendChild(card);
+});
