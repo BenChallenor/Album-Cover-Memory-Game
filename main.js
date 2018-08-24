54e99994a153418eb0c00bc0bf618bb095105506
Ben <ben.challenor@btopenworld.com>
2018-08-24 16:39:37 +0100

    Add cards array

---
+++ main.js
@@ -0,0 +1,49 @@
+const cardsArray = [{
+    'name': 'purple-rain'
+    'img': 'images/purplerain.jpeg'
+  },
+  {
+    'name': '1999'
+    'img': 'images/1999.jpeg'
+  },
+  {
+    'name': 'around-the-world-in-a-day'
+    'img': 'images/aroundtheworldinaday.jpeg'
+  },
+  {
+    'name': 'controversy'
+    'img': 'images/contoversy.jpeg'
+  },
+  {
+    'name': 'diamonds-and-pearls'
+    'img': 'images/diamondsandpearls.jpeg'
+  },
+  {
+    'name': 'dirty-mind'
+    'img': 'images/dirtymind.jpeg'
+  },
+  {
+    'name': 'graffiti-bridge'
+    'img': 'images/graffitibridge.jpeg'
+  },
+  {
+    'name': 'lovesexy'
+    'img': 'images/lovesexy.jpeg'
+  },
+  {
+    'name': 'parade'
+    'img': 'images/parade.jpeg'
+  },
+  {
+    'name': 'prince'
+    'img': 'images/prince.jpeg'
+  },
+  {
+    'name': 'sign-o-the-times'
+    'img': 'images/signothetimes.jpeg'
+  },
+  {
+    'name': 'symbol'
+    'img': 'images/symbol.jpeg'
+  },
+];
